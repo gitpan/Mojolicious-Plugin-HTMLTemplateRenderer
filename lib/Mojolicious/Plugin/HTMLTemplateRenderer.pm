2e5c08afe6f0df41914df27ea7987f8e819ade2b
Bob Faist <bfaist@cpan.org>
2012-11-02 16:26:14 +0000

    Import of BFAIST/Mojolicious-Plugin-HTMLTemplateRenderer-0.03 from CPAN.
    gitpan-cpan-distribution: Mojolicious-Plugin-HTMLTemplateRenderer
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         BFAIST/Mojolicious-Plugin-HTMLTemplateRenderer-0.03.tar.gz
    gitpan-cpan-author:       BFAIST
    gitpan-cpan-maturity:     released

--- lib/Mojolicious/Plugin/HTMLTemplateRenderer.pm
+++ lib/Mojolicious/Plugin/HTMLTemplateRenderer.pm
@@ -1,25 +1,35 @@
 package Mojolicious::Plugin::HTMLTemplateRenderer;
+
 use Mojo::Base 'Mojolicious::Plugin';
 
 use HTML::Template;
 
-our $VERSION = '0.01';
+our $VERSION = '0.03';
 
 sub register {
-  my ($self, $app) = @_;
+  my ($self, $app, $conf) = @_;
 
   $app->renderer->add_handler(
      tmpl => sub {
         my ($r, $c, $output, $options) = @_;
 
         my $path = $r->template_path($options);
+	my $controller = $c->stash('controller');
+
+	my @template_dirs;
+
+	push @template_dirs, $c->app->home->rel_dir('templates');
+
+	if($controller) {
+            push @template_dirs, $c->app->home->rel_dir("templates/$controller");
+	}
 
         my %t_options;
 
         $t_options{die_on_bad_params} = 0;
         $t_options{global_vars} = 1;
         $t_options{loop_context_vars} = 1;
-        $t_options{path} = [ $c->app->home->rel_dir('templates') ];
+        $t_options{path} = \@template_dirs;
         $t_options{search_path_on_include} = 1;
 
         if(defined($options->{inline})) {
@@ -31,9 +41,11 @@ sub register {
 
         my %tmpl_params = %{$c->stash};
 
-        my $t = HTML::Template->new(%t_options);
+        my $t = HTML::Template->new(%t_options,
+                                    %{$conf->{tmpl_new_opts}             || {}},
+                                    %{delete $tmpl_params{tmpl_new_opts} || {}});
 
-	unless($t) { die "ERROR: No template created"; }
+        unless($t) { $r->render_exception("ERROR: No template created"); }
 
         $t->param(%tmpl_params);
 
@@ -54,8 +66,19 @@ Mojolicious::Plugin::HTMLTemplateRenderer - Mojolicious Plugin
   # Mojolicious
   $self->plugin('HTMLTemplateRenderer');
 
+
   # Mojolicious::Lite
   plugin 'HTMLTemplateRenderer';
+
+  # Render HTML::Template handler and post 'utf8 => 1' option for next HTML::Template->new call
+  get '/' => sub{
+    my $self = shift;
+    $self->render('bender', handler => 'tmpl', tmpl_new_opts => {utf8 => 1});
+  }
+
+
+  # Set default options for all HTML::Template->new calls
+  plugin 'HTMLTemplateRenderer', tmpl_new_opts => {blind_cache => 1, open_mode => '<:encoding(UTF-16)'};
 
 =head1 DESCRIPTION
 
